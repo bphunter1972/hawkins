ee31aa84e064c0e0dc2bdec2343f822413031035
Brian Hunter <brian.p.hunter@gmail.com>
2015-12-15 14:58:37 -0800

    Remove dpi obj dir.

--- verif/vkits/uvm/1_2/src/dpi/obj/unitVcs/uvm_dpi.d
+++
@@ -1,7 +0,0 @@
-obj/unitVcs/uvm_dpi.o obj/unitVcs/uvm_dpi.d: uvm_dpi.cc obj/unitVcs/uvm_dpi.h.ts \
- /nfs/cacadtools/synopsys/vcs_mx/H-2013.06-SP1-10/include/vpi_user.h \
- /nfs/cacadtools/synopsys/vcs_mx/H-2013.06-SP1-10/include/veriuser.h \
- /nfs/cacadtools/synopsys/vcs_mx/H-2013.06-SP1-10/include/svdpi.h \
- uvm_common.c uvm_regex.cc uvm_hdl.c uvm_hdl_vcs.c \
- /nfs/cacadtools/synopsys/vcs_mx/H-2013.06-SP1-10/include/vcsuser.h \
- uvm_svcmd_dpi.c
